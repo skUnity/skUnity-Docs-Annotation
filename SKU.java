3cdbedfafe92bc560066e8b1eb5bbad95dc15784
Joshua Roy <10731363+WheezyGold7931@users.noreply.github.com>
2018-01-04 12:54:58 -0500

    Fix indents

--- SKU.java
+++ SKU.java
@@ -7,8 +7,8 @@ import java.lang.annotation.RetentionPolicy;
 public class SKU {
 	
 	static {
-		 final String defaultPackage = new String(
-                    new byte[]{'c', 'o', 'm', '.', 's', 'k', 'u', 'n', 'i', 't','y' , '.', 'd', 'o', 'c', 's'});
+	 final String defaultPackage = new String(
+	    new byte[]{'c', 'o', 'm', '.', 's', 'k', 'u', 'n', 'i', 't','y' , '.', 'd', 'o', 'c', 's'});
             if (SKU.class.getPackage().getName().equals(defaultPackage)) {
                 throw new IllegalStateException("skUnity Docs Implementation is in the default package! Please move it to your package!");
             }
